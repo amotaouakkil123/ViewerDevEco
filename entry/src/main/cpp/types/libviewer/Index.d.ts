7f806130c77593006f77ab51792950ccf85187da
a00914048 <adam.motaouakkil@huawei.com>
2025-04-23 10:26:50 -0400

    Finalize ui cleanup implementation

---
+++ entry/src/main/cpp/types/libviewer/Index.d.ts
@@ -0,0 +1,6 @@
+import { NodeContent } from '@ohos.arkui.node';
+
+export const createNativeNode: (content: NodeContent) => void;
+export const setGanesh: () => void;
+export const setGraphiteVulkan: () => void;
+export const setGraphiteDawn: () => void;
